604c628796d6a9514428efde4c4e66d0eff06551
Gilbert Torchon <gilbert@noukod.com>
2019-01-10 19:02:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    session["item"]=@params[:item]
+    session[@params.keys[0]]=@params[:item]
     @session=session
     erb :item
   end
